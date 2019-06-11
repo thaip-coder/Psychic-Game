29c02fd8f608313524c789a0edb74123fdafad01
Thai Phan <thaiphan90@yahoo.com>
2019-06-10 22:34:56 -0500

    added javascript

---
+++ assets/javascript/main.js
@@ -0,0 +1,12 @@
+//Letters that can be guessed
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Animal word bank
+var animalBank = ["Horse", "Eagle", "Giraffe", "Elephant", "Tiger", "Snake", "Crow", "Monkey", "Panther", "Armadillo"];
+
+//User Guess
+
+
+//Computer chooses random animal from animalBank array
+var compChoice = compChoice[Math.floor(Math.random() * compChoice.length)];
+
