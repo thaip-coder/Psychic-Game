4ae9604c31c5e1451f24ef4f44c74219c54239fa
Thai Phan <thaiphan90@yahoo.com>
2019-06-10 23:20:54 -0500

    update js and html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,7 @@
-//Letters that can be guessed
+//Letters array
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//Starting values
+//Default settings
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
@@ -10,4 +10,6 @@ var repeatLetters = null;
 
 //Computer chooses random letter.
 var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+
 
