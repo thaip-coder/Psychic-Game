c08e4ecd87b668cfed1b261cc42ee7b64b9da801
Thai Phan <thaiphan90@yahoo.com>
2019-06-12 14:54:04 -0500

    updated javascript

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,7 @@
 //Letters array
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//Default settings
+//Default variables
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
@@ -10,6 +10,46 @@ var repeatLetters = null;
 
 //Computer chooses random letter.
 var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+//Displays the number of guesses remaining 
+function guessCounter() {
+	document.querySelector("#guessesLeft").innerHTML = "Remaining Guesses: " + guessesLeft;
+}
+
+//Displays letters that have been guessed
+function guessHistory() {
+	document.querySelector("#history").innerHTML = "History: " + usedLetters.join(' ');
+}
+
+//Restarts the game with a new computer generated letter guess after a win or loss
+var restart = function() {
+    guessesLeft = 9;
+    usedLetters = [];
+    var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+
+//When a key is pushed down
+document.onkeydown = function(event) {
+    guessesLeft--;
+
+    //Determines which key is pressed
+    var userGuess = event.key;
+
+    usedLetters.push(userGuess);
+    guessCounter();
+	guessHistory();
+    
+    if (userGuess === randomLetter) {
+        wins++;
+        document.querySelector("#wins").innerHTML = "Wins: " + wins;
+        restart();
+    }else if (guessesLeft === 0) {
+        losses++;
+        document.querySelector("#losses").innerHTML = "Losses: " + losses;
+        restart();
+    }
+
+};
 
 
 
